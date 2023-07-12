048ce544a4aebfecd29d75ce35af5de465188dca
Zane Werle <zanewerle@gmail.com>
2023-07-12 15:18:12 -0500

    added in the addReaction and deleteReaction functions into the thoughtController.js

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -66,4 +66,39 @@ module.exports = {
             res.status(500).json(err);
         }
     },
+    async addReaction(req, res) {
+        try {
+            const thought = await Thought.findOneAndUpdate(
+                { _id: req.params.thoughtId },
+                { $push: { reactions: body } },
+                { runValidators: true, new: true }
+            );
+
+            if (!thought) {
+                return res.status(404).json({ message: 'No thought with this id!' });
+            }
+
+            res.json(thought);
+        } catch (err) {
+            console.log(err);
+            res.status(500).json(err);
+        }
+    },
+    async deleteReaction(req, res) {
+        try {
+            const thought = await Thought.findOneAndUpdate(
+                { _id: req.params.thoughtId },
+                { $pull: { reactions: params.reactionId } },
+                { runValidators: true, new: true }
+            );
+
+            if (!thought) {
+                return res.status(404).json({ message: 'No thought with this id!' });
+            }
+
+            res.json(thought);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
 };
