466b8dd790b7b6033c95eabcd555292189d25490
Zane Werle <zanewerle@gmail.com>
2023-07-12 15:20:24 -0500

    added the reaction functions into the thoughtsRoutes and routed them properlly

--- routes/api/thoughtsRoutes.js
+++ routes/api/thoughtsRoutes.js
@@ -6,10 +6,14 @@ const {
     createThought,
     updateThought,
     deleteThought,
+    addReaction,
+    deleteReaction,
 } = require('../../controllers/thoughtController');
 
 router.route('/').get(getAllThoughts).post(createThought);
 
 router.route('/:thoughtId').get(getSingleThought).put(updateThought).delete(deleteThought);
+
+router.route('/:thoughtId/reactions').post(addReaction).delete(deleteReaction);
 
 module.exports = router;
